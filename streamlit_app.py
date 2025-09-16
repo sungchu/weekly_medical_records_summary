1156394f82621c4caa05920831108ab39847b221
sung-chu <songzhu1030@gmail.com>
2025-09-16 15:07:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ else:
         dept_choice = st.selectbox("請選擇科部", ["內科部"])
 
     # 根據科部讀取對應檔案
-    department_file = {"內科部": "filtered_MED.jsonl"}
-    df = pd.read_json(department_file[dept_choice], lines=True)
+    department_file = {"內科部": "SURG_10input.xlsx"}
+    df = pd.read_excel(department_file[dept_choice])
     #df = pd.read_json(os.path.join(base_dir, "data", department_file[dept_choice]), lines=True)
 
     # 在主頁面選擇範例
